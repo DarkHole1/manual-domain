41ba01c93b5cd788db3b12190cfada917cbd8f0b
Dark Hole <dark.hole1@yandex.ru>
2022-08-15 21:22:24 +0300

    Add more verbose message

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 type Configuration struct {
 	File      string
 	Domain    string
+	Host      string
 	Command   string
 	BackupDir string
 }
@@ -36,6 +37,8 @@ func ReadConfig() Configuration {
 type FT struct {
 	IPs     string
 	Message string
+	Host    string
+	Zone    string
 }
 
 func ReadCurrentIPs(conf Configuration) string {
@@ -46,11 +49,11 @@ func ReadCurrentIPs(conf Configuration) string {
 	}
 	res := []string{}
 	for _, line := range strings.Split(string(data), "\n") {
-		if strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tA\t", conf.Domain)) {
-			res = append(res, strings.TrimPrefix(line, fmt.Sprintf("%s\tIN\tA\t", conf.Domain)))
+		if strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tA\t", conf.Host)) {
+			res = append(res, strings.TrimPrefix(line, fmt.Sprintf("%s\tIN\tA\t", conf.Host)))
 		}
-		if strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tAAAA\t", conf.Domain)) {
-			res = append(res, strings.TrimPrefix(line, fmt.Sprintf("%s\tIN\tAAAA\t", conf.Domain)))
+		if strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tAAAA\t", conf.Host)) {
+			res = append(res, strings.TrimPrefix(line, fmt.Sprintf("%s\tIN\tAAAA\t", conf.Host)))
 		}
 	}
 	return strings.Join(res, "\n")
@@ -67,13 +70,13 @@ func main() {
 			err := r.ParseForm()
 			if err != nil {
 				log.Printf("Error occured: %s\n", err.Error())
-				page.Execute(w, FT{currentIps, "Случилась непонятная внутренняя ошибка"})
+				page.Execute(w, FT{currentIps, "Случилась непонятная внутренняя ошибка", conf.Host, conf.Domain})
 				return
 			}
 
 			ips := r.PostForm.Get("ips")
 			if ips == "" {
-				page.Execute(w, FT{currentIps, "Запрос пустой или некорректный"})
+				page.Execute(w, FT{currentIps, "Запрос пустой или некорректный", conf.Host, conf.Domain})
 				return
 			}
 			ipsSplitted := strings.Split(ips, "\n")
@@ -85,7 +88,7 @@ func main() {
 				}
 				ipParsed := net.ParseIP(ipCleaned)
 				if ipParsed == nil {
-					page.Execute(w, FT{currentIps, "Я не смог разобрать IP \"" + ipCleaned + "\""})
+					page.Execute(w, FT{currentIps, "Я не смог разобрать IP \"" + ipCleaned + "\"", conf.Host, conf.Domain})
 					return
 				}
 				ipsParsed = append(ipsParsed, ipParsed)
@@ -94,26 +97,26 @@ func main() {
 			res := ""
 			for _, ip := range ipsParsed {
 				if ip.To4() != nil {
-					res += fmt.Sprintf("%s\tIN\tA\t%s\n", conf.Domain, ip.String())
+					res += fmt.Sprintf("%s\tIN\tA\t%s\n", conf.Host, ip.String())
 				} else {
-					res += fmt.Sprintf("%s\tIN\tAAAA\t%s\n", conf.Domain, ip.String())
+					res += fmt.Sprintf("%s\tIN\tAAAA\t%s\n", conf.Host, ip.String())
 				}
 			}
 
 			data, err := os.ReadFile(conf.File)
 			if err != nil {
 				log.Printf("Error occured: " + err.Error())
-				page.Execute(w, FT{currentIps, "Случилась непонятная внутренняя ошибка"})
+				page.Execute(w, FT{currentIps, "Случилась непонятная внутренняя ошибка", conf.Host, conf.Domain})
 				return
 			}
 			err = os.WriteFile(path.Join(conf.BackupDir, path.Base(conf.File)+"."+time.Now().Format("2006-01-02-15-04-05")), data, 0644)
 			if err != nil {
 				log.Printf("Error occured: %s\n", err.Error())
-				page.Execute(w, FT{currentIps, "Случилась ошибка при создании бэкапа"})
+				page.Execute(w, FT{currentIps, "Случилась ошибка при создании бэкапа", conf.Host, conf.Domain})
 			}
 			newLines := []string{}
 			for _, line := range strings.Split(string(data), "\n") {
-				if strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tA\t", conf.Domain)) || strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tAAAA\t", conf.Domain)) {
+				if strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tA\t", conf.Host)) || strings.HasPrefix(line, fmt.Sprintf("%s\tIN\tAAAA\t", conf.Host)) {
 					continue
 				}
 				newLines = append(newLines, line)
@@ -121,7 +124,7 @@ func main() {
 			err = os.WriteFile(conf.File, []byte(strings.Join(newLines, "\n")+res), 0644)
 			if err != nil {
 				log.Printf("Error occured: " + err.Error())
-				page.Execute(w, FT{currentIps, "Случилась непонятная внутренняя ошибка"})
+				page.Execute(w, FT{currentIps, "Случилась непонятная внутренняя ошибка", conf.Host, conf.Domain})
 				return
 			}
 			currentIps = ""
@@ -132,14 +135,14 @@ func main() {
 			if out, err := cmd.CombinedOutput(); err != nil {
 				log.Printf("Error occured: %s", err)
 				log.Printf("Output: %s", string(out))
-				page.Execute(w, FT{currentIps, "Не получилось перезапустить DNS, смотрите в логи"})
+				page.Execute(w, FT{currentIps, "Не получилось перезапустить DNS, смотрите в логи", conf.Host, conf.Domain})
 				return
 			}
-			page.Execute(w, FT{currentIps, "Всё прошло хорошо"})
+			page.Execute(w, FT{currentIps, "Всё прошло хорошо", conf.Host, conf.Domain})
 			return
 		}
 
-		page.Execute(w, FT{currentIps, ""})
+		page.Execute(w, FT{currentIps, "", conf.Host, conf.Domain})
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
